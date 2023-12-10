c88fde9ffc64e558060988fdd60e3782f541f835
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-12-10 21:42:48 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import "../app.css"
+</script>
+
+<div class="h-screen w-screen overflow-auto bg-gradient-to-br from-yellow-300 to-rose-300">
+	<nav class="m-4 flex flex-row gap-4 text-xl">
+		<a href="/">Home</a>
+		<a href="/posts">Posts</a>
+	</nav>
+
+	<div class="">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="flex justify-center text-2xl">Welcome!</h1>

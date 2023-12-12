3fdccb34d439fb8a463fc33da657215c242b6ed0
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-12-12 10:04:49 +0100

    initiated Prisma SQLite database using dummy data

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import "../app.css"
 </script>
 
-<div class="h-screen w-screen overflow-auto bg-gradient-to-br from-yellow-300 to-rose-300">
-	<nav class="m-4 flex flex-row gap-4 text-xl">
+<div class="h-screen w-screen overflow-auto bg-gradient-to-br from-slate-900 to-pink-400">
+	<nav class="m-8 flex flex-row gap-4 text-xl text-white">
 		<a href="/">Home</a>
 		<a href="/posts">Posts</a>
 	</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,1 @@
-<h1 class="flex justify-center text-2xl">Welcome!</h1>
+<h1 class="flex justify-center text-2xl text-white">Welcome!</h1>
